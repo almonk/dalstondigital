e4394ee81a1be735df1e18dd8b6daa9e1e4c3916
tim harding <tim.harding@gmail.com>
2011-03-16 23:31:27 +0000

    Bare rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dalstondigital::Application.config.secret_token = 'b0721e02871f9ca1fa45ddbb432a33cf000e9cd7a72220ca7ca746cb4c61deca2e09a2bfd653311c9e1acd099af6cdffc2aab3d318392766187de1146dd4a743'
